55438d1800a2696ea963f4493f7c2fe67db96ec5
Wesley Casselman <w-casselman@onu.edu>
2023-11-02 09:23:27 -0400

    Final Solution

--- Lab Problem 10.2/P10-2.cpp
+++ Lab Problem 10.2/P10-2.cpp
@@ -1,3 +1,60 @@
 // P10-1
 // Wesley Casselman
 // All Souls' Day of 2023
+// 
+// Copied from P8-2
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int count_words(string str);
+
+int main()
+{
+	while (true)
+	{
+		string str;
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, str);
+
+		if (str == "Q")
+		{
+			break;
+		}
+
+		cout << "Word count: " << count_words(str) << endl;
+	}
+
+	return 0;
+}
+
+int count_words(string str)
+{
+	int words = 0;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (i == 0)
+		{
+			if (str[0] != ' ')
+			{
+				words++;
+			}
+		}
+		else if (i == (str.length() - 1))
+		{
+
+		}
+		else
+		{
+			if (str[i] == ' ' && str[i + 1] != ' ')
+			{
+				words++;
+			}
+		}
+	}
+
+	return words;
+}
